88f4eac47595f4132ee2272caba21b8de2b6245e
EntityPENGIN <nopedoritos@gmail.com>
2023-03-18 18:03:36 +0900

    header test 2

---
+++ header.js
@@ -0,0 +1,11 @@
+function header(){
+    $.ajax(
+        {
+            url: "header.html",
+            cache: false,
+            success: function(html){
+                document.write(html);
+            }
+        }
+    );
+}
